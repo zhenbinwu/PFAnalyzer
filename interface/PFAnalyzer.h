7af321254f88ade0c36f222fa35ecf3dbd1ffa5b
Ben@nbay04 <benwu@fnal.gov>
2014-01-22 21:46:01 -0600

    Update the PFAnalyzer in ugly version
    Well, I wasted some time trying to split things into plugins..
    But it became too messy in the debugging.... Goes this way hopefully
    it won't be too ugly ...

--- interface/PFAnalyzer.h
+++ interface/PFAnalyzer.h
@@ -41,9 +41,24 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "TH1.h"
+#include "DataFormats/JetReco/interface/GenJetCollection.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/CaloMET.h"
+#include "DataFormats/METReco/interface/PFMETCollection.h"
+#include "DataFormats/METReco/interface/PFMET.h"
+#include "DataFormats/METReco/interface/GenMETCollection.h"
+#include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/Math/interface/deltaR.h"
+//
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 //
 // class declaration
 //
+//
+
+//#include "UserCode/PFAnalyzer/plugins/AnaRecHit.h"
+//#include "UserCode/PFAnalyzer/plugins/AnaPFJet.h"
 
 class PFAnalyzer : public edm::EDAnalyzer {
    public:
@@ -51,10 +66,23 @@ class PFAnalyzer : public edm::EDAnalyzer {
       ~PFAnalyzer();
 
       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-      bool BookTH1D(std::string name, std::string title, unsigned nbins, double xlow, double xhigh);
+      //bool BookTH1D(std::string name, std::string title, unsigned nbins, double xlow, double xhigh);
+      int RecHitAna();
       int GetInputTag(const edm::ParameterSet& iConfig);
       int GetHandleByLabel(const edm::Event& iEvent);
 
+      // ----------member data ---------------------------
+      edm::Service<TFileService> fs;
+
+
+   protected:
+      int PFJetAna();
+      int CaloJetAna();
+      int GenJetAna();
+      bool HasNeutrino() const;
+      int CaloMETAna();
+      int GenMETAna();
+      int PFMetAna();
 
    private:
       virtual void beginJob() override;
@@ -72,7 +100,7 @@ class PFAnalyzer : public edm::EDAnalyzer {
       //----------------------------------------------------------------------------
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ RecHit ~~~~~
       edm::InputTag HbHeRecHitTag_;
-      edm::Handle<edm::SortedCollection<HBHERecHit,edm::StrictWeakOrdering<HBHERecHit> > >  HbHeRecHitHdl;
+      edm::Handle<HBHERecHitCollection>  HbHeRecHitHdl;
 
       edm::InputTag HfRecHitTag_;
       edm::Handle<edm::SortedCollection<HFRecHit,edm::StrictWeakOrdering<HFRecHit> > >  HfRecHitHdl;
@@ -107,14 +135,83 @@ class PFAnalyzer : public edm::EDAnalyzer {
       edm::InputTag PSPFClusterTag_;
       edm::Handle<std::vector<reco::PFCluster> >  PSPFClusterHdl;
 
-      edm::InputTag pfJetInputTag_;
-      edm::Handle<reco::PFJetCollection> pfJetcollection;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PFJet ~~~~~
+      edm::InputTag GenJetInputTag_;
+      edm::Handle<reco::GenJetCollection> GenJetHdl;
+
+      edm::InputTag PFJetInputTag_;
+      edm::Handle<reco::PFJetCollection> PFJetHdl;
+
+      edm::InputTag CaloJetInputTag_;
+      edm::Handle<reco::CaloJetCollection> CaloJetHdl;
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PFMET ~~~~~
+      edm::InputTag GenMETInputTag_;
+      edm::Handle<reco::GenMETCollection> GenMETHdl;
+
+      edm::InputTag PFMETInputTag_;
+      edm::Handle<reco::PFMETCollection> PFMETHdl;
+
+      edm::InputTag CaloMETInputTag_;
+      edm::Handle<reco::CaloMETCollection> CaloMETHdl;
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GenParticle ~~~~~
+      edm::InputTag GenParticleInputTag_;
+      edm::Handle<reco::GenParticleCollection> GenParticleHdl;
+//
       
       //now do what ever initialization is needed
-      edm::Service<TFileService> fs;
       unsigned int minTracks_;
-      std::map<std::string, TH1*> Hist1D;
+
+      //----------------------------------------------------------------------------
+      //  Rechit
+      //----------------------------------------------------------------------------
       TH1D* demohisto; 
+      TH1D* RH_Energy;
+
+      //----------------------------------------------------------------------------
+      //  PFJet
+      //----------------------------------------------------------------------------
+
+      TH1D* PFJet_Pt;
+      TH1D* PFJet_Eta;
+      TH1D* PFJet_Phi;
+      
+      TH1D* CaloJet_Pt;
+      TH1D* CaloJet_Eta;
+      TH1D* CaloJet_Phi;
+
+      TH1D* GenJet_Pt;
+      TH1D* GenJet_Eta;
+      TH1D* GenJet_Phi;
+
+      TH1D* PFJetRes_Pt;
+      TH1D* PFJetRes_Eta;
+      TH1D* PFJetRes_Phi;
+      TH1D* GenJetRes_Pt;
+      TH1D* GenJetRes_Eta;
+      TH1D* GenJetRes_Phi;
+
+      //----------------------------------------------------------------------------
+      //  MET
+      //----------------------------------------------------------------------------
+
+      TH1D* PFMET_Pt;
+      TH1D* PFMET_Eta;
+      TH1D* PFMET_Sig;
+      
+      TH1D* CaloMET_Pt;
+      TH1D* CaloMET_Eta;
+      TH1D* CaloMET_Sig;
+
+      TH1D* GenMET_Pt;
+      TH1D* GenMET_Eta;
+      TH1D* GenMET_Sig;
+
+
+      //AnaRecHit *RecHitAna;
+      //AnaPFJet *PFJetAna;
 };
 
 //define this as a plug-in
--- src/PFAnalyzer.cc
+++ src/PFAnalyzer.cc
@@ -16,6 +16,13 @@
 //
 //
 #include "UserCode/PFAnalyzer/interface/PFAnalyzer.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+#include "DataFormats/HcalDetId/interface/HcalElectronicsId.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
 
 //
 // constants, enums and typedefs
@@ -32,11 +39,46 @@ minTracks_(iConfig.getUntrackedParameter<unsigned int>("minTracks",0))
 {
    //now do what ever initialization is needed
   demohisto = fs->make<TH1D>("tracks" , "Tracks" , 100 , 0 , 5000 );
-  BookTH1D("HbheRechit", "Number of HbHeRechit", 1000, 0, 3000);
+  RH_Energy = fs->make<TH1D>("Rechit_energy" , "Rechit_Energy" , 100 , 0 , 5000 );
 
+  PFJet_Pt = fs->make<TH1D>("PFJet_PT", "PF Jet_Pt " ,  100, 0, 1000);
+  PFJet_Eta =fs->make<TH1D>("PFJet_Eta", "PF Jet Eta", 100, -5, 5);
+  PFJet_Phi = fs->make<TH1D>("PFJet_Phi", "PF jet Phi", 100, -5, 5);
+
+  PFJetRes_Pt = fs->make<TH1D>("PFJet_PT", "PF Jet_Pt " ,  100, 0, 1000);
+  PFJetRes_Eta =fs->make<TH1D>("PFJet_Eta", "PF Jet Eta", 100, -5, 5);
+  PFJetRes_Phi = fs->make<TH1D>("PFJet_Phi", "PF jet Phi", 100, -5, 5);
+
+  GenJet_Pt = fs->make<TH1D>("GenJet_PT", "Gen Jet_Pt " ,  100, 0, 1000);
+  GenJet_Eta =fs->make<TH1D>("GenJet_Eta", "Gen Jet Eta", 100, -5, 5);
+  GenJet_Phi = fs->make<TH1D>("GenJet_Phi", "Gen jet Phi", 100, -5, 5);
+
+  GenJetRes_Pt = fs->make<TH1D>("GenJet_PT", "Gen Jet_Pt " ,  100, 0, 1000);
+  GenJetRes_Eta =fs->make<TH1D>("GenJet_Eta", "Gen Jet Eta", 100, -5, 5);
+  GenJetRes_Phi = fs->make<TH1D>("GenJet_Phi", "Gen jet Phi", 100, -5, 5);
+
+  CaloJet_Pt = fs->make<TH1D>("CaloJet_PT", "Calo Jet_Pt " ,  100, 0, 1000);
+  CaloJet_Eta =fs->make<TH1D>("CaloJet_Eta", "Calo Jet Eta", 100, -5, 5);
+  CaloJet_Phi = fs->make<TH1D>("CaloJet_Phi", "Calo jet Phi", 100, -5, 5);
   // Get all the input tag needed
-  GetInputTag(iConfig);
+  PFMET_Pt = fs->make<TH1D>("PFMET_PT", "PF MET_Pt " ,  100, 0, 1000);
+  PFMET_Eta =fs->make<TH1D>("PFMET_Eta", "PF MET Eta", 100, -5, 5);
+  PFMET_Sig = fs->make<TH1D>("PFMET_Sig", "PF MET Sig", 100, -5, 5);
 
+  GenMET_Pt = fs->make<TH1D>("GenMET_PT", "Gen MET_Pt " ,  100, 0, 1000);
+  GenMET_Eta =fs->make<TH1D>("GenMET_Eta", "Gen MET Eta", 100, -5, 5);
+  GenMET_Sig = fs->make<TH1D>("GenMET_Sig", "Gen MET Sig", 100, -5, 5);
+
+  CaloMET_Pt = fs->make<TH1D>("CaloMET_PT", "Calo MET_Pt " ,  100, 0, 1000);
+  CaloMET_Eta =fs->make<TH1D>("CaloMET_Eta", "Calo MET Eta", 100, -5, 5);
+  CaloMET_Sig = fs->make<TH1D>("CaloMET_Sig", "Calo MET Sig", 100, -5, 5);
+
+
+
+
+  GetInputTag(iConfig);
+  //PFJetAna->Test();
+  //RecHitAna->SetupHandler(HbHeRecHitHdl, HfRecHitHdl, HoRecHitHdl);
 }
 
 
@@ -57,30 +99,42 @@ PFAnalyzer::~PFAnalyzer()
 void
 PFAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+   GetHandleByLabel(iEvent);
    using namespace edm;
 
-   GetHandleByLabel(iEvent);
    Handle<reco::TrackCollection> tracks;
    iEvent.getByLabel("generalTracks", tracks); 
-   iEvent.getByLabel("ak8PFJets", pfJetcollection); 
    demohisto->Fill(tracks->size());
+   //PFJetAna->SetupHandler(pfJetcollection.product());
 
    if (minTracks_ <= tracks->size())
    {
      LogInfo("Demo") << "number of tracks "<<tracks->size();
    }
 
-  for (unsigned int i = 0; i < pfJetcollection->size(); ++i)
-  {
-    reco::PFJet j = pfJetcollection->at(i);
-    std::cout << j.energy() << std::endl;
-  }
-  std::cout<<"Run to \033[0;31m"<<__func__<<"\033[0m at \033[1;36m"<< __FILE__<<"\033[0m, line \033[0;34m"<< __LINE__<<"\033[0m"<< std::endl; 
-  for(unsigned int i=0; i < HcalPFRecHitHdl->size(); i++)
-  {
-    reco::PFRecHit HcalPFRH = HcalPFRecHitHdl->at(i);
-    std::cout << HcalPFRH.energy() << std::endl;
-  }
+  //for (unsigned int i = 0; i < pfJetcollection->size(); ++i)
+  //{
+    //reco::PFJet j = pfJetcollection->at(i);
+    //std::cout << j.energy() << std::endl;
+  //}
+
+
+  PFJetAna();
+  GenJetAna();
+  CaloJetAna();
+  //PFJetAna->Analyze();
+  //RecHitAna->Analyze();
+  //for(unsigned int i=0; i < HcalPFRecHitHdl->size(); i++)
+  //{
+    //reco::PFRecHit HcalPFRH = HcalPFRecHitHdl->at(i);
+    //std::cout << HcalPFRH.energy() << std::endl;
+  //}
+
+
+
+
+    //std::cout << HcalPFRH.energy() << std::endl;
+  //}
 
 
 }
@@ -88,8 +142,12 @@ PFAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
-PFAnalyzer::beginJob()
+PFAnalyzer::beginJob(const edm::Event& iEvent)
 {
+   //RecHitAna.AnaRecHit(HbHeRecHitHdl, HfRecHitHdl, HoRecHitHdl);
+
+   //RecHitAna->BookHistogram(fs);
+   //PFJetAna->BookHist(fs);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -142,16 +200,6 @@ PFAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
 }
 
 // ===  FUNCTION  ============================================================
-//         Name:  PFAnalyzer::BookTH1D
-//  Description:  
-// ===========================================================================
-bool PFAnalyzer::BookTH1D(std::string name, std::string title, unsigned nbins, double xlow, double xhigh)
-{
-  Hist1D[name] = fs->make<TH1D>(name.c_str(), title.c_str(), nbins , xlow, xhigh);
-  return true;
-}       // -----  end of function PFAnalyzer::BookTH1D  -----
-
-// ===  FUNCTION  ============================================================
 //         Name:  PFAnalyzer::GetConfigFlag
 //  Description:  
 // ===========================================================================
@@ -175,7 +223,15 @@ int PFAnalyzer::GetInputTag(const edm::ParameterSet& iConfig)
   HOPFClusterTag_ = iConfig.getParameter<edm::InputTag>("HOPFClusterTag");
   PSPFClusterTag_ = iConfig.getParameter<edm::InputTag>("PSPFClusterTag");
 
+  GenJetInputTag_ = iConfig.getParameter<edm::InputTag>("GenJetInputTag");
+  CaloJetInputTag_ = iConfig.getParameter<edm::InputTag>("CaloJetInputTag");
+  PFJetInputTag_ = iConfig.getParameter<edm::InputTag>("PFJetInputTag");
 
+  GenMETInputTag_ = iConfig.getParameter<edm::InputTag>("GenMETInputTag");
+  CaloMETInputTag_ = iConfig.getParameter<edm::InputTag>("CaloMETInputTag");
+  PFMETInputTag_ = iConfig.getParameter<edm::InputTag>("PFMETInputTag");
+
+  GenParticleInputTag_= iConfig.getParameter<edm::InputTag>("GenParticleInputTag");
   //caloJetInputTag_ = iConfig.getParameter<edm::InputTag>("caloJetInputTag");
 
   return 1;
@@ -204,5 +260,217 @@ int PFAnalyzer::GetHandleByLabel(const edm::Event& iEvent)
    iEvent.getByLabel(HcalPFClusterTag_, HcalPFClusterHdl); 
    iEvent.getByLabel(HOPFClusterTag_, HOPFClusterHdl); 
    iEvent.getByLabel(PSPFClusterTag_, PSPFClusterHdl); 
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PFJet ~~~~~
+   iEvent.getByLabel(GenJetInputTag_, GenJetHdl); 
+   iEvent.getByLabel(PFJetInputTag_, PFJetHdl); 
+   iEvent.getByLabel(CaloJetInputTag_, CaloJetHdl); 
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PFMET ~~~~~
+   iEvent.getByLabel(GenMETInputTag_, GenMETHdl); 
+   iEvent.getByLabel(PFMETInputTag_, PFMETHdl); 
+   iEvent.getByLabel(CaloMETInputTag_, CaloMETHdl); 
+
+   iEvent.getByLabel(GenParticleInputTag_, GenParticleHdl); 
   return 1;
 }       // -----  end of function PFAnalyzer::GetHandleByLabel  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFAnalyzer::RecHitAna
+//  Description:  
+// ===========================================================================
+int PFAnalyzer::RecHitAna()
+{
+
+  //for (HBHERecHitCollection::const_iterator j=HbHeRecHitHdl->begin(); j != HbHeRecHitHdl->end(); j++) {
+    //HcalDetId cell(j->id());
+    //const CaloCellGeometry* cellGeometry =
+      //geometry->getSubdetectorGeometry (cell)->getGeometry (cell) ;
+    //double eta  = cellGeometry->getPosition().eta () ;
+    //double phi  = cellGeometry->getPosition().phi () ;
+    //double zc   = cellGeometry->getPosition().z ();
+    //int sub     = cell.subdet();
+    //int depth   = cell.depth();
+    //int inteta  = cell.ieta();
+    //if(inteta > 0) inteta -= 1;
+    //int intphi  = cell.iphi()-1;
+    //double en   = j->energy();
+    //double t    = j->time();
+    //int stwd    = j->flags();
+    //int auxstwd = j->aux();
+  //}
+
+  return 1;
+}       // -----  end of function PFAnalyzer::RecHitAna  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFAnalyzer::PFJetAna
+//  Description:  
+// ===========================================================================
+int PFAnalyzer::PFJetAna()
+{
+  if (!PFJetHdl.isValid())
+  {
+    return 0;
+  }
+  std::cout << " size" << PFJetHdl->size() << std::endl;
+  for(reco::PFJetCollection::const_iterator it=PFJetHdl->begin();
+    it!=PFJetHdl->end(); it++)
+  {
+    
+    PFJet_Pt->Fill(it->pt());
+    PFJet_Eta->Fill(it->eta());
+    PFJet_Phi->Fill(it->phi());
+  }
+
+
+  std::vector<reco::GenJet> genjet_temp;
+  for(unsigned int i=0; i < GenJetHdl->size(); i++)
+  {
+    reco::GenJet jet = GenJetHdl->at(i);
+    genjet_temp.push_back(jet);
+  }
+
+  for(unsigned int i=0; i < PFJetHdl->size(); i++)
+  {
+    reco::PFJet jet = PFJetHdl->at(i);
+    int found = -1;
+    for(unsigned int j=0; j < genjet_temp.size(); j++)
+    {
+      DeltaR<reco::PFJet, reco::GenJet> deltaR;
+      reco::GenJet gjet = genjet_temp.at(j);
+      //if (jt.p4().DeltaR(gjet.p4()) < 0.3)
+      if ( deltaR(jet, gjet) < 0.3)
+      {
+        PFJetRes_Pt->Fill(jet.pt());
+        PFJetRes_Eta->Fill(jet.eta());
+        PFJetRes_Phi->Fill(jet.phi());
+        GenJetRes_Pt->Fill(gjet.pt());
+        GenJetRes_Eta->Fill(gjet.eta());
+        GenJetRes_Phi->Fill(gjet.phi());
+        found = signed(j);
+        break;
+      }
+    }
+    if (found != -1)
+    {
+      genjet_temp.erase(genjet_temp.begin()+found);
+    }
+  }
+
+  return 1;
+}       // -----  end of function PFAnalyzer::PFJetAna  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFJetAna::GenJetAna
+//  Description:  
+// ===========================================================================
+int PFAnalyzer::GenJetAna()
+{
+  if (!GenJetHdl.isValid())
+  {
+    return 0;
+  }
+  std::cout << " size" << GenJetHdl->size() << std::endl;
+  for(reco::GenJetCollection::const_iterator it=GenJetHdl->begin();
+    it!=GenJetHdl->end(); it++)
+  {
+    
+    GenJet_Pt->Fill(it->pt());
+    GenJet_Eta->Fill(it->eta());
+    GenJet_Phi->Fill(it->phi());
+  }
+  return 1;
+}       // -----  end of function PFJetAna::GenJetAna  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFJetAna::CaloJetAna
+//  Description:  
+// ===========================================================================
+int PFAnalyzer::CaloJetAna()
+{
+  if (!CaloJetHdl.isValid())
+  {
+    return 0;
+  }
+  std::cout << " size" << CaloJetHdl->size() << std::endl;
+  for(reco::CaloJetCollection::const_iterator it=CaloJetHdl->begin();
+    it!=CaloJetHdl->end(); it++)
+  {
+    
+    CaloJet_Pt->Fill(it->pt());
+    CaloJet_Eta->Fill(it->eta());
+    CaloJet_Phi->Fill(it->phi());
+  }
+  return 1;
+}       // -----  end of function PFJetAna::CaloJetAna  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFAnalyzer::HasNeutrino
+//  Description:  
+// ===========================================================================
+bool PFAnalyzer::HasNeutrino() const
+{
+  for(std::vector<reco::GenParticle>::const_iterator it=GenParticleHdl->begin();
+    it!=GenParticleHdl->end(); it++)
+  {
+    if (fabs(it->pdgId()) ==  12  || fabs(it->pdgId()) ==  14  || fabs(it->pdgId()) ==  16)
+    {
+      if (fabs(it->mother()->pdgId()) == 24)
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}       // -----  end of function PFAnalyzer::HasNeutrino  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFAnalyzer::PFMetAna
+//  Description:  
+// ===========================================================================
+int PFAnalyzer::PFMetAna()
+{
+  for(reco::PFMETCollection::const_iterator it=PFMETHdl->begin();
+    it!=PFMETHdl->end(); it++)
+  {
+    PFMET_Pt->Fill(it->sumEt());
+    PFMET_Eta->Fill(it->eta());
+    PFMET_Sig->Fill(it->mEtSig());
+  }
+
+
+  return 1;
+}       // -----  end of function PFAnalyzer::PFMetAna  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFMETAna::GenMETAna
+//  Description:  
+// ===========================================================================
+int PFAnalyzer::GenMETAna()
+{
+  for(unsigned int i=0; i < GenMETHdl->size(); i++)
+  {
+    reco::GenMET MET = GenMETHdl->at(i);
+    GenMET_Pt->Fill(MET.sumEt());
+    GenMET_Eta->Fill(MET.eta());
+    GenMET_Sig->Fill(MET.mEtSig());
+  }
+  return 1;
+}       // -----  end of function PFMETAna::GenMETAna  -----
+
+// ===  FUNCTION  ============================================================
+//         Name:  PFMETAna::CaloMETAna
+//  Description:  
+// ===========================================================================
+int PFAnalyzer::CaloMETAna()
+{
+  for(unsigned int i=0; i < CaloMETHdl->size(); i++)
+  {
+    reco::CaloMET MET = CaloMETHdl->at(i);
+    CaloMET_Pt->Fill(MET.sumEt());
+    CaloMET_Eta->Fill(MET.eta());
+    CaloMET_Sig->Fill(MET.mEtSig());
+  }
+  return 1;
+}       // -----  end of function PFMETAna::CaloMETAna  -----
